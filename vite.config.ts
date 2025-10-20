f14125fbd7b34c1fff7e3a09194461da61f1b5bb
ohowe <ohowe@duck.com>
2025-10-20 01:07:33 -0700

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: process.env.BASE_PATH || "/",
 });
